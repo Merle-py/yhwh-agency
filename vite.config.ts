1b74c1716d49f6725b7a4a74ad4c5b11018fe737
Merle <131203879+merlepy@users.noreply.github.com>
2025-04-21 19:12:44 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: '/yhwh-agency/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
